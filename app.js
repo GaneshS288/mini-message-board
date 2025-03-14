71220a4656a1f8dd69bd03b553e3a44fa58ec549
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-03-14 13:47:57 +0530

    chore: remove whitespace

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const port = 8080;
 const app = express();
 const assetsPath = path.join(import.meta.dirname, "public");
 
-
-
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 
