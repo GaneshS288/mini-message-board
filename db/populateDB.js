8ff8981d93f47eddbe8205a270de27ed89817fa2
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-03-25 10:49:48 +0530

    feat: add id and date columns to populate db schema

--- db/populateDB.js
+++ db/populateDB.js
@@ -4,9 +4,9 @@ import "dotenv/config";
 const { Client } = pg;
 
 const data = [
-  { username: "Bob", text: "Javascript is a pain" },
-  { username: "Ganesh", text: "need more SQL practice" },
-  { username: "borgar", text: "I am a borgar" },
+  { username: "Bob", text: "Javascript is a pain", date: new Date() },
+  { username: "Ganesh", text: "need more SQL practice", date: new Date() },
+  { username: "borgar", text: "I am a borgar", date: new Date() },
 ];
 
 const SQL = `CREATE TABLE IF NOT EXISTS users (
@@ -15,8 +15,10 @@ const SQL = `CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id INTEGER REFERENCES users(id),
-    text VARCHAR (255) NOT NULL
+    text VARCHAR (255) NOT NULL,
+    date DATE NOT NULL
 );
 `;
 
@@ -32,10 +34,16 @@ async function populate(data) {
 
   await client.connect();
   await client.query(SQL);
-  for(let item of data) {
-    const { rows } = await client.query("INSERT INTO users (name) VALUES ($1) RETURNING id", [item.username]);
+  for (let item of data) {
+    const { rows } = await client.query(
+      "INSERT INTO users (name) VALUES ($1) RETURNING id",
+      [item.username]
+    );
     console.log(rows);
-    await client.query("INSERT INTO MESSAGES (user_id, text) VALUES($1, $2)", [rows[0].id, item.text])
+    await client.query(
+      "INSERT INTO MESSAGES (user_id, text, date) VALUES($1, $2, $3)",
+      [rows[0].id, item.text, item.date.toDateString()]
+    );
   }
   await client.end();
   console.log("done");
